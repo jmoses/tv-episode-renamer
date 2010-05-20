4c932f9684c0a3fc20ef30c1d94090ece5807637
Jon Moses <jon@burningbush.us>
2010-05-20 08:36:03 -0400

    Better options

--- run.rb
+++ run.rb
@@ -1,30 +1,40 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'rubygems'
+require 'trollop'
 
-series_word, nice_series = ARGV[0..1]
-
-if ! series_word or ! nice_series or series_word == '' or nice_series == ''
-  STDERR.puts "Series word and nice series are required"
-  exit 1
+opts = Trollop::options do 
+	opt :prefix, "Prefix of shows to find.", :type => :string
+	opt :title, "'Pretty' title of series (used on new file names)", :type => :string
+	opt :series_regex, "Different regex to use.", :type => :string
+	opt :pretend, "Don't really do anything", :default => false
 end
 
-series_patt = %r|S([\d]{1,2})E([\d]{1,2})|i
+if opts[:prefix] == '' or opts[:title] == ''
+  Trollop.die("Prefix and Title are required.")
+end
+
+series_patt = opts[:series_regex] ? Regexp.new(opts[:series_regex]): %r|S([\d]{1,2})E([\d]{1,2})|i
 
 Dir['*'].each do |fname|
   next if File.directory?(fname)
-  
-  if fname =~ /#{series_word}/i
+
+  if fname =~ /#{opts[:prefix]}/i
     if fname =~ series_patt
-      puts "Found Season #{$1}, episode #{$2} of #{nice_series}"
+      puts "Found Season #{$1}, episode #{$2} of #{opts[:title]}"
       
-      new_name = "#{nice_series} - S#{sprintf('%02d', $1.to_i)}E#{sprintf('%02d', $2.to_i)}#{File.extname(fname)}"
+      new_name = "#{opts[:title]} - S#{sprintf('%02d', $1.to_i)}E#{sprintf('%02d', $2.to_i)}#{File.extname(fname)}"
       
       if new_name == fname
         puts " -> properly named already"
       else
         puts " -> #{new_name}"
-        FileUtils.move( fname, new_name )
+        if File.exists?(new_name)
+          STDERR.puts(" -> exists!")
+        else
+          FileUtils.move( fname, new_name, :noop => opts[:pretend] )
+        end
       end
     end
   end
